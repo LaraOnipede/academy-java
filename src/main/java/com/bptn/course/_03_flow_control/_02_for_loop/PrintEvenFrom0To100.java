daf33ce5d58e09f7e9132aece51f2dca1fff4ff6
Lara Onipede <thelaraonipede@gmail.com>
2024-09-27 10:47:41 -0400

    allfiles for day 4 of academy added

--- src/main/java/com/bptn/course/_03_flow_control/_02_for_loop/PrintEvenFrom0To100.java
+++ src/main/java/com/bptn/course/_03_flow_control/_02_for_loop/PrintEvenFrom0To100.java
@@ -1,20 +1,17 @@
 package com.bptn.course._03_flow_control._02_for_loop;
 
-public class PrintEvenFrom0To100 {  
-    public static void main(String args[]) {  
-        int evenNumber = 1; 
-
-        for (int i=2; i<=100; i+=2) {
-            System.out.println(i);
-        }
- 
-        System.out.print("Printing the  even numbers from 1 to 100: \n"); 
-
-        //Approach 2
-        for (int i=1; i<=100; i++) {
+public class PrintEvenFrom0To100 {
+    public static void main(String[] args) {
+        // Declare variable 'i' and initialize it to 2, the first even number
+        int i = 2;
+        
+        // Start a loop where 'i' is incremented by 1 each time, with a condition to stop at 100
+        for (i = 1; i <= 100; i++) {
+            // Check if 'i' is an even number
             if (i % 2 == 0) {
+                // If true, print 'i' as it is an even number
                 System.out.println(i);
             }
         }
-    }  
+    }
 }
--- src/main/java/com/bptn/course/_04_arrays/Array.java
+++ src/main/java/com/bptn/course/_04_arrays/Array.java
@@ -1,5 +1,7 @@
 package com.bptn.course._04_arrays;
 
+import java.util.Arrays;
+
 public class Array {
 
 	public static void main(String[] args) {
